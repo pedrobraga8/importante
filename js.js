eb9ac536dee3c89de28180679315471c9ac31682
pedrobraga8 <114931801+pedrobraga8@users.noreply.github.com>
2024-07-24 22:34:09 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    startConfetti();
-});
-
-function startConfetti() {
-    const confettiCount = 100;
-    const confettiColors = ['#ff69b4', '#ff1493', '#ffb6c1', '#db7093'];
-    for (let i = 0; i < confettiCount; i++) {
-        createConfetti(confettiColors[Math.floor(Math.random() * confettiColors.length)]);
+document.getElementById('showMessageBtn').addEventListener('click', function() {
+    var messageDiv = document.getElementById('message');
+    if (messageDiv.classList.contains('hidden')) {
+        messageDiv.classList.remove('hidden');
+        this.textContent = 'Esconder Mensagem';
+    } else {
+        messageDiv.classList.add('hidden');
+        this.textContent = 'Mostrar Mensagem';
     }
-}
-
-function createConfetti(color) {
-    const confetti = document.createElement('div');
-    confetti.classList.add('confetti');
-    confetti.style.backgroundColor = color;
-    confetti.style.left = `${Math.random() * 100}vw`;
-    confetti.style.top = `${Math.random() * -100}vh`;
-    confetti.style.animationDuration = `${Math.random() * 3 + 2}s`;
-    document.body.appendChild(confetti);
-}
+});
