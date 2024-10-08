09db613f8a608e3b65893ee44a7bf58f3cfd5d7a
pedrobraga8 <114931801+pedrobraga8@users.noreply.github.com>
2024-10-08 14:32:13 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,1 @@
-document.getElementById('showMessageBtn').addEventListener('click', function() {
-    var messageDiv = document.getElementById('message');
-    if (messageDiv.classList.contains('hidden')) {
-        messageDiv.classList.remove('hidden');
-        this.textContent = 'Esconder Mensagem';
-    } else {
-        messageDiv.classList.add('hidden');
-        this.textContent = 'Mostrar Mensagem';
-    }
-});
+//js
